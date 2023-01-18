e43087d98b3b5afaee1da0c8a1f8d737724f684f
Ethan Smith <EthanKSmith@gmail.com>
2023-01-18 15:17:28 -0700

    Assignment Complete

--- src/components/Colors.jsx
+++ src/components/Colors.jsx
@@ -3,9 +3,11 @@ import { useParams } from "react-router";
 
 function Colors() {
     const { Word, Color1, Color2 } = useParams();
+
+
   return (
     <div>
-        <h1 >The Word is: {Word}</h1>
+        <h1 style={{color:Color1, backgroundColor:Color2}}>The Word is: {Word}</h1>
         <h2>{Color1}</h2>
         <h2>{Color2}</h2>
     </div>
